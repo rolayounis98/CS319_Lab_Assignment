beb4ac1cdf1ecf6147b9c1fa07f2ff73eab95d69
Rola <rola.younis@ug.bilkent.ed.tr>
2020-04-07 12:25:31 +0300

    Main class created and functionality shown.

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> ArrayList1 = new ArrayList<>();
+        ArrayList<Integer> ArrayList2= new ArrayList<>();
+        ArrayList<Integer> ArrayList3 = new ArrayList<>();
+        
+        ArrayList1.add()
+        ArrayOrganizer organizer = new ArrayOrganizer[ArrayList1, ArrayList2, ArrayList3];
+
+        System.out.println("result =>" + organizer);
+
+        
+
+
+
+
+
+    }
+}
